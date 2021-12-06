35e54be5cd6de1c7403e38bb73a4b80590897bda
Tolibjon <tolibjohn@gmail.com>
2021-12-06 19:32:17 +0500

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  root 'static#index'
   namespace :v1, defaults: {format:JSON} do 
     get 'greetings', to:'greetings#index'
   end
+
+  get '*page', to:'static#index', constraints:->(req) do
+    !req.xhr? && req.format.html?
+  end
+
+  root 'static#index'
+  
 end
