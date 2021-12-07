e7ab151936cb9173fb0764c380a1bd4e183eed2c
Tolibjon <tolibjohn@gmail.com>
2021-12-07 18:59:41 +0500

    Add route

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,16 +2,23 @@ import React from "react"
 import PropTypes from "prop-types"
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+
 import Greeting from './Greeting'
+
+import configureStore from "../configureStore";
+const store = configureStore()
 
 class App extends React.Component {
   render () {
     return (
-      <BrowserRouter>
-        <Routes>
-          <Route exact path='/' element={<Greeting greeting='Hi there' />} />
-        </Routes>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path='/' element={<Greeting />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
